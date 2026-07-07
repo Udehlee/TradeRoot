cf7ac94004e8abf62b7084e7b5d63dab6d500fd9
udehlee <ritzeepita@gmail.com>
2026-07-07 22:21:03 +0100

    feat(components): add popular products component

---
+++ app/components/PopularProducts.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const PopularProducts = () => {
+    return (
+        <section className="bg-white py-4">
+        <div className="container">
+            <div className="flex justify-between  items-center mb-5 ">
+                <div className="col1 w-[30%]">
+                    <h2 className="text-[20px] text-gray-800 font-[700]">popular products</h2>
+
+                </div>
+            </div>
+        </div>
+        </section>
+    )
+}
+
+
+export default PopularProducts
